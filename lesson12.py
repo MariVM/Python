2fd5a32cfbcaaef8ff6db10febe6e88279cddcb5
MariVM <marinavm21@gmail.com>
2018-10-25 00:39:15 +0300

    Restaurant “At Python’s”

---
+++ lesson12.py
@@ -0,0 +1,19 @@
+#Restaurant “At Python’s”
+	
+import random
+
+dishes_string = input('What would you like? ') # This is a string
+#dishes_string = 'eggs, spam, eggs, spam, tomato juice, spam'
+   
+if len(dishes_string): 
+	print('Please see time to cook dishes: ')
+
+dishes_list = dishes_string.split(',')
+dishes_name = []
+for dishes in dishes_list:
+    dishes_name.append(dishes.strip().capitalize())
+    dishes_name_2 = set(dishes_name)
+     
+for dishes in dishes_name_2:
+    random_integer = str(random.randint(0, 60))
+    print(dishes + '.' * (30-len(dishes)) + random_integer + 'min')
