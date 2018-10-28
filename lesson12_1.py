ff0f3649a311088dd7229a206d65641820943256
MariVM <marinavm21@gmail.com>
2018-10-29 00:45:52 +0200

    used comprehension in code

---
+++ lesson12_1.py
@@ -0,0 +1,12 @@
+import random
+
+dishes_string = input('What would you like? ') # This is a string
+#dishes_string = 'eggs, spam, eggs, spam, tomato juice, spam'
+
+if len(dishes_string): 
+	print('Please see time to cook dishes: ')
+
+dishes_list = set(dishes_string.split(' '))
+dishes = []
+
+dishes = [print(dishes.title().strip().capitalize(), '.' * (30-len(dishes)), random.randint (0, 60), 'min') for dishes in dishes_list]
