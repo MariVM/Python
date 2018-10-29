b11bab214fc8ed478d95d66e17d83b40cde80f2f
MariVM <marinavm21@gmail.com>
2018-10-29 16:22:49 +0200

    updated code with comprehension

--- lesson12_1.py
+++ lesson12_1.py
@@ -6,7 +6,7 @@ dishes_string = input('What would you like? ') # This is a string
 if len(dishes_string): 
 	print('Please see time to cook dishes: ')
 
-dishes_list = set(dishes_string.split(' '))
-dishes = []
+#dishes_list = set(dishes_string.split(' '))
+#dishes = []
 
-dishes = [print(dishes.title().strip().capitalize(), '.' * (30-len(dishes)), random.randint (0, 60), 'min') for dishes in dishes_list]
+dishes = [print(dishes.title().strip().capitalize(), '.' * (30-len(dishes)), random.randint (0, 60), 'min') for dishes in set(dishes_string.split(' '))]
